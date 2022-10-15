6a660cd4629a300bef66888c01172191a647ca73
temiladeyemo <temilademariam99@gmail.com>
2022-10-15 15:32:43 -0700

    Hello, world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - prints a piece of art is useful, Dora Korpar, 2015-10-19
+ *Return: return 1 if code runs successfully
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 61);
+	return (1);
+}

4b604b813efce1292136d7f0c0e50dd82fad9776
temiladeyemo <temilademariam99@gmail.com>
2022-10-15 12:37:43 -0700

    Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+   /**
+    ** main - print a line of string and appends to a new line
+    ** Return: 0 (success)
+    */
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle");
+	return (0);
+}
+

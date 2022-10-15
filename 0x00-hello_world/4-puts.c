adbc8110b25fe84a6edb3e64a943a9716685dd0a
temiladeyemo <temilademariam99@gmail.com>
2022-10-15 13:42:22 -0700

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-   /**
-    ** main - print a line of string and appends to a new line
-    ** Return: 0 (success)
-    */
-int main(void)
+/**
+ *main- puts print a line of string and apends to a newline
+ *return: always return 0 for a successfully run code
+ */
+
+int void main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building multilingual puzzle");
 	return (0);
 }
-

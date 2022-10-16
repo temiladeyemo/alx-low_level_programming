0e81e746f660e54149510bdb705f9e1bc627bfa9
temiladeyemo <temilademariam99@gmail.com>
2022-10-16 15:18:15 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 	int num;
+	char letter;
 
 	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
-	int letter;
 
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
